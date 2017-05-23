e07c32d974cd1c76f7745ce44cc3bbc3bc21787c
xiaobai87 <262957316@qq.com>
2017-05-23 17:16:25 +0800

    Signed-off-by: xiaobai87 <262957316@qq.com>

--- admin/navIncHead.php
+++ admin/navIncHead.php
@@ -25,7 +25,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $row['muser'] ?> <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">退出</a></li>
+            <li><a href="loginout.php">退出</a></li>
           </ul>
         </li>
       </ul>
